848aa1caac51bdbd87a9b93f7e8ec2463a78fc68
Roberta Diesi <92572831+robertadiesi@users.noreply.github.com>
2022-10-17 17:55:43 +0200

    Update R_code_ecointeract.r

--- R_code_ecointeract.r
+++ R_code_ecointeract.r
@@ -2,7 +2,7 @@
 
 # We are using the sp package. To install it use:
 # install.packages("sp")
-library(sp) # you can also make use of require ()
+library(sp) # you can also make use of require (), it is usefull to make additional pieces of R
 
 # we are using meuse:
 # search for: "meuse dataset R sp package"
@@ -26,8 +26,7 @@ plot(meuse$cadmium, meuse$zinc)
 cad <- meuse$cadmium   # we make a new object "cad" to make things more simple
 zin <- meuse$zinc
 plot(cad,zin)
-
-attach(meuse) # attach is used to access the variables present in the data framework without calling the data frame
+attach(meuse) # attach is used to access the variables present in the data framework without calling the data frame. If we use attach, we don't need to use the $ simbol
 plot(cadmium,zinc)
 # detach (meuse)
 
@@ -35,4 +34,24 @@ plot(cadmium,zinc)
 # we use "pairs"
 pairs(meuse) # to create a pairwise correlation plot
 
+meuse[,3:6] # to select specific variables/columns
 
+pol <- meuse[,3:6] 
+head(pol) # to show the first 6 columns
+pairs(pol, col="blue", cex=1.5)
+
+# let's use the names of the columns:
+
+# in R ~ (tilde) means =  
+pairs(~cadmium + copper + lead + zinc, data=meuse)
+
+panel.correlations <- function (x, y, digits=1, prefix="", cex.cor)
+  
+coordinates(meuse)= ~x+y       
+
+spplot(meuse, "zinc", main="Concentration of zinc")  #spplot is designed to combine a standard R plot with a legend representing a (continuous) color scale
+# main is the title of the plot
+
+spplot(meuse, c("copper","zinc","lead")) 
+
+bubble(meuse, "zinc")
