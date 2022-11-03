4a39a9258e0b29f64401a9765fcaf3fb57837473
Roberta Diesi <92572831+robertadiesi@users.noreply.github.com>
2022-11-03 16:17:38 +0100

    Update R_code_point_pattern.r

--- R_code_point_pattern.r
+++ R_code_point_pattern.r
@@ -23,3 +23,39 @@ cl <- colorRampPalette(c('yellow','orange','red'))(100) # to change the map's co
 cl <- colorRampPalette(c("cyan", "coral", "chartreuse")) (50)
 plot(density_map, col = cl)
 points(covid_planar)
+
+
+# Install the rgdal package by:
+# install.packages("rgdal")
+library(rgdal)   # GDAL is a translator library for raster and vector geospatial data formats
+setwd("C:/Users/Rober/Desktop/lab")
+#let's add the coastlines
+coastlines <- readOGR("ne_10m_coastline.shp")
+covid <- read.table("covid_agg.csv", header=T)
+covid
+head(covid)
+attach(covid)
+covid_planar <- ppp(x=lon, y=lat, c(-180, 180), c(-90,90))
+plot(covid_planar)
+plot(coastlines,add=T)
+density_map <- density(covid_planar)
+plot(density_map)
+points(covid_planar, pch=18, col="green")
+plot(coastlines, add=T)
+plot(coastlines, add=T, col="yellow")
+
+cl <- colorRampPalette(c("bisque", "azure", "brown"))(100)
+plot(density_map, col=cl)
+points(covid_planar, pch=18)
+plot(coastlines, add=T)
+
+
+ #suing interpolation
+attach(covid)
+marks(covid_planar) <- cases
+
+
+cases_map <- Smooth(covid_planar)
+plot(cases_map, col=cl)
+points(covid_planar)
+plot(coastlines, add=T)
