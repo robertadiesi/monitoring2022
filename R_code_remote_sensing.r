d9ea691999d6845e2f1902a06d8024b032e4bf83
Roberta Diesi <92572831+robertadiesi@users.noreply.github.com>
2022-11-10 16:48:00 +0100

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,11 @@
+# R code for remote sensing data analysis in ecosystem monitoring
+
+library(raster)
+library(RStoolbox)
+
+install.packages(c("raster","RStoolbox"))
+> library(RStoolbox)
+Errore: caricamento pacchetto o namespace fallito per ‘RStoolbox’ in loadNamespace(i, c(lib.loc, .libPaths()), versionCheck = vI[[i]]):
+ non c'è alcun pacchetto chiamato ‘recipes’
+In aggiunta: Messaggio di avvertimento:
+il pacchetto ‘RStoolbox’ è stato creato con R versione 4.2.2 
