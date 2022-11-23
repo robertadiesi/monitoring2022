6741359dd6c1603920c85e686c54c1824394cb5e
Roberta Diesi <92572831+robertadiesi@users.noreply.github.com>
2022-11-23 14:54:05 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -65,3 +65,33 @@ par(mfrow=c(2,1))
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Hist")
 
+### back in time 
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_1988, r=3, g=4, b=2, stretch="Lin")
+
+# exercise: make a multiframe with 2 rows and 1 column plotting the 1988 and the 2011 images
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=2, b=4, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+# calculating the difference between images
+# multitemporal analysis 
+difnir <- p224r63_1988[[4]] - p224r63_2011[[4]]
+cl <- colorRampPalette(c('green','blue','yellow'))(100) #
+plot(difnir,col=cl)
+
+# difference vegetation Index
+# recent DVI (2011)
+DVI2011 <- p224r63_2011[[4]] - p224r63_2011[[3]]
+DVI1988 <- p224r63_1988[[4]] - p224r63_1988[[3]]
+
+difDVI <- DVI1988 - DVI2011
+cl <- colorRampPalette(c('blue','white','red'))(100) #
+plot(difDVI, col=cl)
