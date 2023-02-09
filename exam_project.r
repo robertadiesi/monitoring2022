f6e3d286c8a16ed3f8b9361d6b44c307148c7df7
Roberta Diesi <92572831+robertadiesi@users.noreply.github.com>
2023-02-09 17:48:42 +0100

    Update exam_project.r

--- exam_project.r
+++ exam_project.r
@@ -18,7 +18,7 @@ library(patchwork)
 
 # I'm going to set the working directory 
 
-setwd("C:/Users/Rober/Desktop/exam_project")
+setwd("C:/Users/Rober/Desktop/exam_project_pirenei")
 
 
 # The aim of this project is to compare the difference in Snow Content Area in the Alps located in the North of Italy between the winter of 2017 and winter 2022
@@ -33,34 +33,40 @@ snow2022 <- raster("c_gls_SCE500_202202280000_CEURO_MODIS_V1.0.1.nc")
 # Now I'm going to use the plot function, that is useful for creating graphs and charts for visualizations
 plot(snow2022)
 
-# Since the area visualized is too wide and I want to focus only on the area of the Alps in the North of Italy, I'm going to crop the image 
+# Since the area visualized is too wide and I want to focus only on the area of the Pirenei, I'm going to crop the image 
 # let's add the coordinates numbers I found on Copernicus to define the region of interest 
-ext <- c(4.29, 16.11, 43.17, 48.61)
+ext <- c(-1.60, 2.89, 41.70, 43.52)
 snow22 <- crop(snow2022, ext)
 
 # now I use the plot function to visualize the new image
 plot(snow22)
 
 # Let's save the new image in PNG format in the folder 
-png(file="image_winter22.png", units="cm", width=30, height=25, res=400)
+png(file="image_winter22.png", units="cm", width=30, height=25, res=500)
 plot(snow22)
 dev.off()
 
 # Now I can import the Copernicus data of winter 2017
 snow2017 <- raster("c_gls_SCE500_201703010000_CEURO_MODIS_V1.0.1.nc")
+plot(snow2017)
 # cropping the image focusing on the same area of interest
 snow17 <- crop(snow2017, ext)
 plot(snow17)
 
 # Save in PNG
-png(file="image_winter17.png", units="cm", width=30, height=25, res=400)
+png(file="image_winter17.png", units="cm", width=30, height=25, res=500)
 plot(snow17)
 dev.off()
 
-p22 <- ggplot() + geom_raster(snow22, mapping = aes(x=x, y=y, fill = Snow.Cover.Extent)) + scale_fill_viridis() + ggtitle("Snow Cover in winter 2022")
-p17 <- ggplot() + geom_raster(snow17, mapping = aes(x=x, y=y, fill = Snow.Cover.Extent)) + scale_fill_viridis() + ggtitle("Snow Cover in winter 2017")
+# Using ggplot function with viridis
+p22 <- ggplot() + geom_raster(snow22, mapping = aes(x=x, y=y, fill = Snow.Cover.Extent)) + scale_fill_viridis() + ggtitle("Snow Cover in 2022")
+p17 <- ggplot() + geom_raster(snow17, mapping = aes(x=x, y=y, fill = Snow.Cover.Extent)) + scale_fill_viridis() + ggtitle("Snow Cover in 2017")
 
-png(file="image_winter17_22.png", units="cm", width=30, height=25, res=400)
+# I want to see both images to compare them 
+p17/p22
+
+# Save it in PNG
+png(file="image_winter17_22.png", units="cm", width=30, height=25, res=500)
 p17 / p22
 dev.off()
 
@@ -74,8 +80,7 @@ plot(dif, col=coldif)
 # in green the snow missing in 2022 with respect to 2017
 
 # saving the file in PNG format
-png(file="diff_winter17_22.png", units="cm", width=30, height=25, res=600)
+png(file="diff_winter17_22.png", units="cm", width=30, height=25, res=500)
 plot(dif, col=coldif)
 dev.off()
-
 
